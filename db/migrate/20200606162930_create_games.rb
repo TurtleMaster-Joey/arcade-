bf2f941454c5b9ed0164a0dfafcc4b3c0d406e06
Kyle <kyledunshee@Kyles-MacBook-Pro.local>
2020-06-06 10:34:51 -0600

    pushed correct database, again

--- db/migrate/20200606162930_create_games.rb
+++ db/migrate/20200606162930_create_games.rb
@@ -1,6 +1,7 @@
 class CreateGames < ActiveRecord::Migration[6.0]
   def change
     create_table :games do |t|
+      t.string :name
       t.belongs_to :account, null: false, foreign_key: true
 
       t.timestamps
